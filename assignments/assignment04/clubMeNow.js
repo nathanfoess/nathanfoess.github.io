e23e47d10765f14345730db32d0086269c678d04
nathanfoess <89933733+nathanfoess@users.noreply.github.com>
2021-10-25 19:51:06 -0400

    Update clubMeNow.js

--- assignments/assignment04/clubMeNow.js
+++ assignments/assignment04/clubMeNow.js
@@ -91,7 +91,7 @@ function displayClubDistanceEntryForm(c) {
 
 // replace the current "clubs" array with the previous one
 function undoLastShot() {
-	clubs = JSON.parse(localStorage.getItem("clubsUndo");
+	clubs = JSON.parse(localStorage.getItem("clubsUndo"));
 	 window.location.href = "clubDistanceList.html";
 }
 
